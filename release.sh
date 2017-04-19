8148199341d77a8f798926efc5922fbce62d9af1
Shomiron DASGUPTA <shomiron@netmonastery.com>
2017-04-19 18:34:26 +0530

    version

--- release.sh
+++ release.sh
@@ -7,7 +7,7 @@ IMAGE=snort
 # ensure we're up to date
 git pull
 # bump version
-echo "$(cat VERSION)" | awk -F. -v OFS=. 'NF==1{print ++$NF}; NF>1{if(length($NF+1)>length($NF))$(NF-1)++; $NF=sprintf("%0*d", length($NF), ($NF+1)%(10^length($NF))); print}' > VERSION
+@echo "$(cat VERSION)" | awk -F. -v OFS=. 'NF==1{print ++$NF}; NF>1{if(length($NF+1)>length($NF))$(NF-1)++; $NF=sprintf("%0*d", length($NF), ($NF+1)%(10^length($NF))); print}' > VERSION
 
 version=`cat VERSION`
 echo "version: $version"
